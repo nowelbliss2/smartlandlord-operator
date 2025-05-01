f25a107c1e7df51ea65da76317185aad32dbdb8b
Nowel Bliss <nowelbliss2@gmail.com>
2025-05-01 02:53:34 +0100

    Updating controller for resizing

--- internal/controller/smartlandlord_controller.go
+++ internal/controller/smartlandlord_controller.go
@@ -122,6 +122,7 @@ func (r *SmartlandlordReconciler) Reconcile(ctx context.Context, req ctrl.Reques
    
     
 	if *smartlandlordDeployment.Spec.Replicas != replicas {
+	  smartlandlordDeployment.Spec.Replicas = &replicas
       if err = r.Update(ctx, smartlandlord); err != nil {
 	    logger.Error(err, "Failed to update Deployment",
 		         "Deployment.Namespace", smartlandlord.Namespace, "Deployment.Name", smartlandlord.Name)
@@ -129,12 +130,16 @@ func (r *SmartlandlordReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 	    if err := r.Get(ctx, req.NamespacedName, smartlandlord); err != nil {
 				logger.Error(err, "Failed to re-fetch Smartlandlord")
 				return ctrl.Result{}, err
-			}
-  
+	    }
+
+        return ctrl.Result{}, err
+
 	  }
 
-			return ctrl.Result{}, err
+	  return ctrl.Result{Requeue: true}, nil
+
 	}
+
 	return ctrl.Result{}, nil
 }
 
